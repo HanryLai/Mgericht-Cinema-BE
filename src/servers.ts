af29a105d15d525ada765d7568061cac24455fce
daenv <daenvle69@gmail.com>
2023-08-13 18:50:11 +0700

    update server

--- src/servers.ts
+++ src/servers.ts
@@ -5,7 +5,6 @@ const port =  process.env.PORT;
 server.listen(port, () => {
   console.log(`Server start on ${port}`);
 });
-/* process.on('SIGINT', () => {
+process.on('SIGINT', () => {
   server.close(() => console.log(`exits server express`));
 });
- */
