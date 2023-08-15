cce4ccb65cd50e0cf1fb6ee15a27a2d3fc021dd1
daenv <daenvle69@gmail.com>
2023-08-15 14:36:04 +0700

    create promotion model

---
+++ src/app/models/promotions/promotion.model.ts
@@ -0,0 +1,21 @@
+import { Schema, Model } from 'mongoose';
+
+export interface PromotionProps {
+    name: string;
+    description: string;
+    discount_code: string;
+    valid_from: string;
+    valid_to: string;
+    create_at: Date;
+}
+
+export const PromotionSchema: Schema<PromotionProps> = new Schema({
+    name: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    discount_code: { type: 'string', required: true },
+    valid_from: { type: 'string', required: true },
+    valid_to: { type: 'string', required: true },
+    create_at: { type: 'Date', required: true },
+});
+
+export const PromotionModel: Model<PromotionProps> = new Model('Promotion', PromotionSchema);
