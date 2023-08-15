70d76b06ab6c392733bb711f1f2aa6e11c15e987
daenv <daenvle69@gmail.com>
2023-08-15 14:56:12 +0700

    create payment model

---
+++ src/app/models/payments/payment.model.ts
@@ -0,0 +1,17 @@
+import { PaymentOptions } from '@/constants/payment-option/payment_options';
+import { Schema, Model, ObjectId } from 'mongoose';
+export interface PaymentProps {
+    booking_id: ObjectId;
+    payment_option: PaymentOptions;
+    payment_date: Date;
+    amount: number;
+}
+
+export const PaymentSchema: Schema<PaymentProps> = new Schema({
+    booking_id: { type: Schema.Types.ObjectId, ref: 'Booking' },
+    payment_option: { type: String, enum: Object.values(PaymentOptions), default: PaymentOptions.CASH, required: true },
+    payment_date: { type: Date, default: Date.now, required: true },
+    amount: { type: Number, default: 0, required: true },
+});
+
+export const PaymentModel: Model<PaymentOptions> = new Model('Payment', PaymentSchema);
