bc1969b78208c976e6e7e2ade2d3853f02215e17
daenv <daenvle69@gmail.com>
2023-09-25 10:44:41 +0700

    remote

--- src/app/routes/index.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-const UserRouter = require('./users/user.routes');
-const AdminRouter = require('./users/admin.routes');
-router.use('/api/users', UserRouter);
-router.use('/api/users', AdminRouter);
-module.exports = router;
