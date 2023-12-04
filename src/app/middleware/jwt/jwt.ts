ae3b41d3b02ed8d7127d3ca58494948f75bf0eb5
HieuLai <ldmhieudev@gmail.com>
2023-12-05 02:57:40 +0700

    check token expire

--- src/app/middleware/jwt/jwt.ts
+++ src/app/middleware/jwt/jwt.ts
@@ -1,8 +1,14 @@
 import { Request } from 'express';
-
+import { jwt } from './jwt.interface';
+import { Jwt, JwtPayload, decode, verify } from 'jsonwebtoken';
 export const getToken = (req: Request): string | Error => {
-   if (req.headers.authorization?.split(' ')[1] != null) {
-      return req.headers.authorization?.split(' ')[1];
+   const token: string | null | undefined = req.headers.authorization?.split(' ')[1];
+
+   if (token != null && token != undefined) {
+      const payload = verify(token, process.env.JWT_TOKEN as string) as JwtPayload; // using decode will error
+      if (payload.exp !== undefined && payload.exp > Date.now() / 1000)
+         throw new Error('Token expired');
+      return token;
    } else {
       return new Error('not have bearer token');
    }
