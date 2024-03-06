ef0a84eea47fbd1dc1d84dacabd582ecbd8b8e5a
daenv <daenvle69@gmail.com>
2024-03-06 11:37:26 +0700

    Add MongoDB database initialization code

---
+++ src/app/databases/init.db.ts
@@ -0,0 +1,32 @@
+import * as mongoose from 'mongoose';
+import config from '../configs/db.config';
+const host = config.db.host || 'localhost';
+const port = config.db.port || 27017;
+const name = config.db.name || 'sleeprDEV';
+const connectString = `mongodb://${host}:${port}/${name}`;
+
+//using design pattern singletons
+class Database {
+    private static instance: Database | null = null;
+    constructor() {
+        this._connect();
+    }
+    private _connect(_type = 'mongodb'): Promise<void> {
+        mongoose.set('debug', true);
+        mongoose.set('debug', { color: true });
+        mongoose.set('strictQuery', false);
+        return mongoose
+            .connect(connectString, { maxPoolSize: 100 })
+            .then(() => console.log('Connected to MongoDB', name))
+            .catch(() => console.log(' connection failed'));
+    }
+    public static getInstance(): Database {
+        if (!Database.instance) {
+            Database.instance = new Database();
+        }
+        return Database.instance;
+    }
+}
+const instanceMongoDB = Database.getInstance();
+
+export default instanceMongoDB;
