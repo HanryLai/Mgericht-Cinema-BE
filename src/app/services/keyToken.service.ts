b0be1d0b2faa9507ce6145cf9090e93f17b6b015
daenv <daenvle69@gmail.com>
2024-03-07 17:44:14 +0700

    Add getPublicKey method to KeyTokenService

--- src/app/services/keyToken.service.ts
+++ src/app/services/keyToken.service.ts
@@ -13,5 +13,13 @@ class KeyTokenService {
             throw new Error(err);
         }
     };
+    static getPublicKey = async (userId: string) => {
+        try {
+            const publicKey = await keyModel.findOne({ user: userId });
+            return publicKey ? publicKey.publicKey : null;
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 }
 export default KeyTokenService;
