2b24cb4cbd089f8058a405fd44db488460c96d1d
daenv <daenvle69@gmail.com>
2024-03-07 23:26:08 +0700

    Add apiKey middleware to userRouter

--- src/app/routes/users/user.router.ts
+++ src/app/routes/users/user.router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import UserController from '../../controllers/users/user.controller';
+import { apiKey } from '../../middleware/auth/auth.check';
 const userRouter = Router();
 
+userRouter.use(apiKey)
 userRouter.post('/registers', UserController.register);
 
 export default userRouter;
