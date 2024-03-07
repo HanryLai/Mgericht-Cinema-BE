a217cf614139b7445c48bf7003df92e3c1f1f7ff
daenv <daenvle69@gmail.com>
2024-03-07 17:44:11 +0700

    Add apiTokenService class

---
+++ src/app/services/apiToken.service.ts
@@ -0,0 +1,7 @@
+class apiTokenService {
+         public static async createKeyToken({ userId, publicKey }: { userId: string; publicKey: string }) {
+                  
+         }
+}
+
+export default apiTokenService;
