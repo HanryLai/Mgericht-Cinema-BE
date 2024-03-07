73b13c8cc73875332cc86034d07bd45bb956b704
daenv <daenvle69@gmail.com>
2024-03-07 23:26:11 +0700

    Add findById method to apiTokenService

--- src/app/services/apiToken.service.ts
+++ src/app/services/apiToken.service.ts
@@ -1,7 +1,12 @@
+import { apiKeyModel } from '../models/apikey.model';
+
 class apiTokenService {
-         public static async createKeyToken({ userId, publicKey }: { userId: string; publicKey: string }) {
-                  
-         }
+    public static findById = async (key: string) => {
+        const keyd = await apiKeyModel.create({ key: 'ssdas', status: true });
+        console.log("keyyy:",keyd);
+        const objKey = await apiKeyModel.findOne({ key, status: true }).lean(true);
+        return objKey;
+    };
 }
 
 export default apiTokenService;
